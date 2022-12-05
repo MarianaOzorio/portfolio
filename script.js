993e1b596e36e6e3e540609bf0ea26f6f6541ee1
marianaozorio <mariana.athayde@gmail.com>
2022-12-05 00:30:43 -0300

    Update navbar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* FLIP SKILLS CARD */
+
 const cards = document.querySelectorAll(".skill-card");
 
 function showSkillCardInformation() {
@@ -7,5 +9,17 @@ function showSkillCardInformation() {
 cards.forEach((card) =>
 card.addEventListener("click", showSkillCardInformation));
 
+/* HIDE/SHOW NAVBAR LOGO */
 
+const logoNav = document.querySelector(".logo-expanded-navbar");
 
+function hideLogo() {
+    logoNav.style.visibility = 'hidden';
+}
+
+function showLogo() {
+    logoNav.style.visibility = 'visible';
+}
+
+document.querySelector(".navbar-toggler").addEventListener("click", hideLogo);
+document.querySelector(".btn-close").addEventListener("click", showLogo);
